4fc3672b542a8121fa4f140238c34f699b78f203
rohanmishra30 <91248195+rohanmishra30@users.noreply.github.com>
2023-01-08 01:48:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function discountpop(){
+document.getElementById("discount-img").classList.toggle("discountimg");
+   document.getElementById("disocount").classList.toggle("discount-head");
+document.getElementById("arrow").classList.toggle("arrow1");
+}
